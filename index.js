106c0c4e30cfa72be34c68979cddcd7e9655b99b
Tristan Brown <tgbrown@stanford.edu>
2018-07-03 15:05:21 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+var recipes = new Object();
+
+function updateObjectWithKeyAndValue(obj, key, value) {
+  return Object.assign({}, obj, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+   obj[key] = value
+  return obj
+}
+
+function delteFromObjectsByKey (object, key)
